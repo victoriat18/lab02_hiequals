5648c79c898db398192618320afbc9ff0d93cc67
victoriat18 <victoriatorres0718@gmail.com>
2026-06-01 18:28:03 -0500

    update lab02.cpp

--- lab02.cpp
+++ lab02.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string> 
 #include <cctype>
+#include <cassert>
 
 using namespace std;
 //Function compares two strings, not minding upper or lower case
@@ -31,15 +32,25 @@ else //strings equal
     return 0;
 }
 //Test cases for function, print
-int main(){
- cout << "The comparison of \"String one\" and \"string two\" returns "
-    << strcmp_case_insensitive("String one", "string two") << "." << endl;
+int main()
+{
+    cout << "The comparison of \"String one\" and \"string two\" returns "
+         << strcmp_case_insensitive("String one", "string two") << "." << endl;
 
     cout << "The comparison of \"String one\" and \"string one\" returns "
-    << strcmp_case_insensitive("String one", "string one") << "." << endl;
+         << strcmp_case_insensitive("String one", "string one") << "." << endl;
 
     cout << "The comparison of \"String two\" and \"string one\" returns "
-    << strcmp_case_insensitive("String two", "string one") << "." << endl;
+         << strcmp_case_insensitive("String two", "string one") << "." << endl;
+
+    // assert tests
+    assert(strcmp_case_insensitive("Hi", "hi") == 0);
+    assert(strcmp_case_insensitive("String one", "string one") == 0);
+    assert(strcmp_case_insensitive("String one", "string two") < 0);
+    assert(strcmp_case_insensitive("String two", "string one") > 0);
+    assert(strcmp_case_insensitive("", "") == 0);
+
+    cout << "all tests passed!" << endl;
 
     return 0;
 }
