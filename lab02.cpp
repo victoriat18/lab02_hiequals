434c525156652ae4c559417a77cf616d7fecae8f
victoriat18 <victoriatorres0718@gmail.com>
2026-06-01 18:14:24 -0500

    summary

--- lab02.cpp
+++ lab02.cpp
@@ -3,30 +3,34 @@
 #include <cctype>
 
 using namespace std;
-
+//Function compares two strings, not minding upper or lower case
 int strcmp_case_insensitive(const string& s1, const string& s2)
 {
     int i = 0;
+    //loop through both strings character by character
     while (i < s1.length() && i < s2.length())
 {
+    //convert both characters to lowercase for comparison
     char c1 = tolower(s1[i]);
     char c2 = tolower (s2[i]);
-
+    //If first string is smaller, return -1 
     if (c1 < c2)
         return -1;
+    //If first string is larger, return 1
     else if (c1 > c2)
         return 1;
 
     i++;
 }
+//If one string is shorter, it is considered 'smaller'
 if (s1.length() < s2.length())
     return -1;
 else if (s1.length() > s2.length())
     return 1;
-else
+else //strings equal
     return 0;
 }
-
+//Test cases for function, print
 int main(){
  cout << "The comparison of \"String one\" and \"string two\" returns "
     << strcmp_case_insensitive("String one", "string two") << "." << endl;
